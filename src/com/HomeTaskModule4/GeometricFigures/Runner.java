d0dd955e3c5cb289c463182c45221e758342e1f0
GalinaViktorovna <gkalenskaya@gmail.com>
2016-07-03 22:56:22 +0300

    .

--- src/com/HomeTaskModule4/GeometricFigures/Runner.java
+++ src/com/HomeTaskModule4/GeometricFigures/Runner.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Runner {
 
-    public static void main(String[] args) throws IOException, NullPointerException {
+    public static void main(String[] args) throws IOException {
         Util.startProgram();
     }
 }
